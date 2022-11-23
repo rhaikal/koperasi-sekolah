2bd7b80e5440fcd81bf547077d97c252b1cbe348
rhaikal <rhaikal101@gmail.com>
2022-11-23 14:19:55 +0700

    menambahkan migration yang dibutuhkan

---
+++ database/migrations/2022_11_03_050933_create_coupons.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->char('code', 6);
+            $table->float('discount', 3, 2);
+            $table->tinyInteger('max_used');
+            $table->string('description', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coupuns');
+    }
+};
---
+++ database/migrations/2022_11_03_052146_create_invoices.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id');
+            $table->string('method');
+            $table->char('status', 1);
+            $table->char('ammount');
+            $table->timestamp('created_at');
+            $table->time('paid_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
