e71fe0f21d2fb617ab0f21d79972ba5d1b983b3a
rhaikal <rhaikal101@gmail.com>
2022-12-20 16:36:59 +0700

    mengubah tampilan register

--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -3,7 +3,11 @@ import GuestLayout from '@/Layouts/GuestLayout';
 import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
 import PrimaryButton from '@/Components/PrimaryButton';
+import SecondaryButton from '@/Components/SecondaryButton';
 import TextInput from '@/Components/TextInput';
+import Label from '@/Components/Label';
+import { FaGithub } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
 import { Head, Link, useForm } from '@inertiajs/inertia-react';
 
 export default function Register() {
@@ -31,87 +35,113 @@ export default function Register() {
     };
 
     return (
-        <GuestLayout>
+        <GuestLayout header='Create account' imgSrc='http://127.0.0.1:8000/img/auth/create-account-office.jpeg' imgDarkSrc='http://127.0.0.1:8000/img/auth/create-account-office-dark.jpeg' imgAlt='Office...'>
             <Head title="Register" />
 
             <form onSubmit={submit}>
                 <div>
-                    <InputLabel forInput="name" value="Name" />
+                    <InputLabel>
+                        <Label>Name</Label>
 
-                    <TextInput
-                        type="text"
-                        name="name"
-                        value={data.name}
-                        className="mt-1 block w-full"
-                        autoComplete="name"
-                        isFocused={true}
-                        handleChange={onHandleChange}
-                        required
-                    />
+                        <TextInput
+                            type="text"
+                            name="name"
+                            value={data.name}
+                            className="mt-1 block w-full"
+                            autoComplete="name"
+                            isFocused={true}
+                            handleChange={onHandleChange}
+                            required
+                        />
 
-                    <InputError message={errors.name} className="mt-2" />
+                        <InputError message={errors.name} className="mt-2" />
+                    </InputLabel>
                 </div>
 
                 <div className="mt-4">
-                    <InputLabel forInput="email" value="Email" />
+                    <InputLabel>
+                        <Label>Email</Label>
 
-                    <TextInput
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        handleChange={onHandleChange}
-                        required
-                    />
+                        <TextInput
+                            type="email"
+                            name="email"
+                            value={data.email}
+                            className="mt-1 block w-full"
+                            autoComplete="username"
+                            handleChange={onHandleChange}
+                            required
+                        />
 
-                    <InputError message={errors.email} className="mt-2" />
+                        <InputError message={errors.email} className="mt-2" />
+                    </InputLabel>
                 </div>
 
                 <div className="mt-4">
-                    <InputLabel forInput="password" value="Password" />
+                    <InputLabel>
+                        <Label>Password</Label>
 
-                    <TextInput
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        handleChange={onHandleChange}
-                        required
-                    />
+                        <TextInput
+                            type="password"
+                            name="password"
+                            value={data.password}
+                            className="mt-1 block w-full"
+                            autoComplete="new-password"
+                            handleChange={onHandleChange}
+                            required
+                        />
 
-                    <InputError message={errors.password} className="mt-2" />
+                        <InputError message={errors.password} className="mt-2" />
+                    </InputLabel>
                 </div>
 
                 <div className="mt-4">
-                    <InputLabel forInput="password_confirmation" value="Confirm Password" />
+                    <InputLabel>
+                        <Label>Confirm Password</Label>
 
-                    <TextInput
-                        type="password"
-                        name="password_confirmation"
-                        value={data.password_confirmation}
-                        className="mt-1 block w-full"
-                        handleChange={onHandleChange}
-                        required
-                    />
+                        <TextInput
+                            type="password"
+                            name="password_confirmation"
+                            value={data.password_confirmation}
+                            className="mt-1 block w-full"
+                            handleChange={onHandleChange}
+                            required
+                        />
 
-                    <InputError message={errors.password_confirmation} className="mt-2" />
+                        <InputError message={errors.password_confirmation} className="mt-2" />
+                    </InputLabel>
                 </div>
 
-                <div className="flex items-center justify-end mt-4">
-                    <Link
-                        href={route('login')}
-                        className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                        Already registered?
-                    </Link>
-
-                    <PrimaryButton className="ml-4" processing={processing}>
-                        Register
+                <div className='block ms-0 mt-4'>
+                    <PrimaryButton processing={processing} className="w-full justify-center bg-indigo-500">
+                        Log in
                     </PrimaryButton>
                 </div>
             </form>
+
+            <hr className='my-8'/>
+
+            <div className='block ms-0 mt-4'>
+                <SecondaryButton processing={processing} className='w-full justify-center hover:ring-1 hover:ring-gray-500'>
+                    <FaGithub className='text-base mr-2' />
+                    Github
+                </SecondaryButton>
+            </div>
+
+            <div className='block ms-0 mt-4'>
+                <SecondaryButton processing={processing} className='w-full justify-center hover:ring-1 hover:ring-gray-500'>
+                    <FcGoogle className='text-base mr-2'/>
+                    Google
+                </SecondaryButton>
+            </div>
+
+            <div className="flex items-center mt-4">
+                <Link
+                    href={route('login')}
+                    className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                    Already registered?
+                </Link>
+            </div>
         </GuestLayout>
     );
 }
