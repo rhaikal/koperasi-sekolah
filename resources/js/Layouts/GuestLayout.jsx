b19e90b5e90bd9f4a8578ce8e95cd8d2ce07f19c
rhaikal <rhaikal101@gmail.com>
2022-12-20 07:05:19 +0700

    mengubah guest layout

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -4,15 +4,11 @@ import { Link } from '@inertiajs/inertia-react';
 
 export default function Guest({ children }) {
     return (
-        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <Link href="/">
-                    <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
-                </Link>
-            </div>
-
-            <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                {children}
+        <div className="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
+            <div className="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
+                <div className="flex flex-col overflow-y-auto md:flex-row">
+                    {children}
+                </div>
             </div>
         </div>
     );
