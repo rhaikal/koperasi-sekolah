30aaafe85b3b0f9cd7ae5ce00f97f37a465cc9f3
rhaikal <rhaikal101@gmail.com>
2022-12-21 09:48:56 +0700

    mengubah font weight header

--- resources/js/Components/Header.jsx
+++ resources/js/Components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Header({ children }) {
     return (
-        <h1 className="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200">
+        <h1 className="mb-4 text-xl font-bold text-gray-700 dark:text-gray-200">
             {children}
         </h1>
     )
