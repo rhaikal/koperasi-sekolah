4ef6b2423189f87251f03ba828b8acffbc146f63
rhaikal <rhaikal101@gmail.com>
2022-12-21 09:03:15 +0700

    menambahkan library flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/js/**/*.jsx',
+        './node_modules/flowbite/**/*.js'
     ],
 
     theme: {
@@ -17,5 +18,5 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [require('@tailwindcss/forms'), require('flowbite/plugin')],
 };
