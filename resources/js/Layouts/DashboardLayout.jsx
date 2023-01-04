cf8cd472141959a69b866f643ee5549285cf1ae4
rhaikal <rhaikal101@gmail.com>
2023-01-04 12:41:34 +0700

    mengubah dashboard layout

--- resources/js/Layouts/DashboardLayout.jsx
+++ resources/js/Layouts/DashboardLayout.jsx
@@ -1,5 +1,5 @@
 import { usePage } from "@inertiajs/inertia-react";
-import React from "react";
+import React, { useEffect } from "react";
 import DashboardNavbar from "../Components/Dashboard/DashboardNavbar";
 import DashboardSidebar from "../Components/Dashboard/Sidebar/DashboardSidebar";
 import Swal from "sweetalert2/dist/sweetalert2.all";
@@ -7,29 +7,37 @@ import Swal from "sweetalert2/dist/sweetalert2.all";
 export default function Dashboard({children, header}) {
     const { flash, auth } = usePage().props
 
-    if(!(_.isEmpty(flash.alert))){
-        const Toast = Swal.mixin({
-            toast: true,
-            position: 'bottom-end',
-            showConfirmButton: false,
-            timer: 3000,
-            timerProgressBar: true,
-        })
+    useEffect(() => {
+        if(!(_.isEmpty(flash.alert))){
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'bottom-end',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+            })
 
-        Toast.fire({
-            icon: flash.alert.icon,
-            title: flash.alert.message
-        })
-    }
+            Toast.fire({
+                icon: flash.alert.icon,
+                title: flash.alert.message
+            })
+        }
+    }, [flash])
+
+    useEffect(() => {
+        const main = document.getElementsByTagName('main')
+        main[0].scrollTop = 0
+    }, [header])
+
 
     return (
         <div className="flex h-screen bg-gray-50 dark:bg-gray-900">
             <DashboardSidebar />
             <div className="flex flex-col flex-1 w-full">
+                <DashboardNavbar name={auth.user.name} />
                 <main className="h-full overflow-y-auto">
-                    <DashboardNavbar name={auth.user.name} />
                     <div className="container px-6 pb-5 mx-auto grid">
-                        <h2 className="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">{header}</h2>
+                        { header && <h2 className="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">{header}</h2>}
                         {children}
                     </div>
                 </main>
