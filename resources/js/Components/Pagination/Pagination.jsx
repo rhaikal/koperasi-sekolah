0088b2b67d162e3bd8496f3a96b89607e4592dbc
rhaikal <rhaikal101@gmail.com>
2023-01-04 11:49:06 +0700

    memperbaiki pagination

--- resources/js/Components/Pagination/Pagination.jsx
+++ resources/js/Components/Pagination/Pagination.jsx
@@ -4,18 +4,20 @@ import { TbArrowNarrowLeft, TbArrowNarrowRight } from "react-icons/tb";
 
 export default function Pagination({links, from, to, total }) {
     const className = "inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-gray-800 hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ";
+    const disableClassName = "inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-gray-700 dark:bg-gray-700 dark:border-gray-700 dark:text-gray-400 cursor-default "
+
     let prevButton
-    if(!links[0].active) {
+    if(!links[1].active) {
         prevButton = <Link className={className + "rounded-l" } as="button" type="button" href={links[0].url}><TbArrowNarrowLeft className="mr-2 text-xl" /> Prev</Link>
     } else {
-        prevButton = <button className={className + "rounded-l"}><TbArrowNarrowLeft className="mr-2 text-xl" /> Prev</button>
+        prevButton = <button disabled className={disableClassName + "rounded-l"}><TbArrowNarrowLeft className="mr-2 text-xl" /> Prev</button>
     }
 
-    let nextButton
-    if(!links[links.length - 1].active) {
+    let nextButton;
+    if(!links[links.length - 2].active) {
         nextButton = <Link className={className + "rounded-r" } as="button" type="button" href={links[links.length - 1].url}>Next <TbArrowNarrowRight className="ml-2 text-xl" /></Link>
     } else {
-        nextButton = <button className={className + "rounded-r"}>Next <TbArrowNarrowRight className="ml-2 text-xl" /></button>
+        nextButton = <button disabled className={disableClassName + "rounded-r"}>Next <TbArrowNarrowRight className="ml-2 text-xl" /></button>
     }
 
     return (
