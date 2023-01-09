8267bfe3f234ee899363368a8f1cf664bdb2bc5b
rhaikal <rhaikal101@gmail.com>
2023-01-09 11:23:16 +0700

    mengubah application logo

--- resources/js/Components/Dashboard/Sidebar/DashboardSidebar.jsx
+++ resources/js/Components/Dashboard/Sidebar/DashboardSidebar.jsx
@@ -5,14 +5,14 @@ import { FaHome, FaSuitcaseRolling } from "react-icons/fa";
 import { BiCategoryAlt } from "react-icons/bi";
 import { BsArchiveFill } from "react-icons/bs";
 import { GiNotebook } from 'react-icons/gi';
+import ApplicationLogo from "@/Components/ApplicationLogo";
 
 export default function Sidebar() {
     return (
         <aside className="z-20 hidden w-64 overflow-y-auto bg-white bg-indigo-800/90 shadow-xl shadow-indigo-500 dark:bg-gray-200 md:block flex-shrink-0">
             <div className="py-4">
-                <Link href={route("dashboard")} className="flex ml-6 text-2xl font-bold text-gray-100 dark:text-indigo-800/90">
-                    <FaSuitcaseRolling className="mr-2 text-3xl"/>
-                    Kopers
+                <Link href={route("dashboard")}>
+                    <ApplicationLogo className={"justify-center text-gray-100 dark:text-indigo-800/90"} />
                 </Link>
                 <ul className="mt-6">
                     <SideLink href={route('dashboard')} active={route().current('dashboard')}><FaHome className="w-5 h-5 mr-4 text-current"/> Dashboard</SideLink>
