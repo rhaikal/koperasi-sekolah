aa996fbe7a3194eb20b15d315eeb56c6e02a6bb4
rhaikal <rhaikal101@gmail.com>
2023-01-19 18:13:47 +0700

    menambahkan fitur mengurangi stock ketika checkout

--- app/Services/CheckoutService.php
+++ app/Services/CheckoutService.php
@@ -4,17 +4,20 @@ namespace App\Services;
 
 use App\Repositories\InvoiceRepository;
 use App\Repositories\OrderRepository;
+use App\Repositories\productRepository;
 use Symfony\Component\Translation\Exception\NotFoundResourceException;
 
 class CheckoutService
 {
     private OrderRepository $orderRepository;
     private InvoiceRepository $invoiceRepository;
+    private productRepository $productRepository;
 
-    public function __construct(OrderRepository $orderRepository, InvoiceRepository $invoiceRepository)
+    public function __construct(OrderRepository $orderRepository, InvoiceRepository $invoiceRepository, productRepository $productRepository)
     {
         $this->orderRepository = $orderRepository;
         $this->invoiceRepository = $invoiceRepository;
+        $this->productRepository = $productRepository;
     }
 
     public function store($data)
@@ -24,7 +27,15 @@ class CheckoutService
         if($order){
             $invoice = $this->invoiceRepository->create($data['method'], $order->id);
 
-            if($invoice) $this->orderRepository->update(['status' => '1'], $order);
+            if($invoice) {
+                foreach($order->products as $product) {
+                    $this->productRepository->update($product, [
+                        'stock' => $product->stock - $product->pivot->quantity
+                    ]);
+                }
+
+                $this->orderRepository->update(['status' => '1'], $order);
+            }
 
             return $invoice;
         } else throw new NotFoundResourceException;
