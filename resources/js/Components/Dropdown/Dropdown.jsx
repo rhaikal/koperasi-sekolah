2d7320d1d0d06280e82ed4c9c97369aea4c9cd07
rhaikal <rhaikal101@gmail.com>
2023-02-02 09:45:33 +0700

    memperbaiki bug dropdown

--- resources/js/Components/Dropdown/Dropdown.jsx
+++ resources/js/Components/Dropdown/Dropdown.jsx
@@ -33,12 +33,12 @@ const Trigger = ({ children }) => {
 const Content = ({ align = 'right', width = '48', containerClasses, contentClasses = 'py-1 bg-white', children }) => {
     const { open, setOpen } = useContext(DropDownContext);
 
-    let alignmentClasses = 'origin-top';
+    let alignmentClasses = 'origin-top ';
 
     if (align === 'left') {
-        alignmentClasses = 'origin-top-left left-0';
+        alignmentClasses += 'origin-top-left left-0';
     } else if (align === 'right') {
-        alignmentClasses = 'origin-top-right right-0';
+        alignmentClasses += 'origin-top-right right-0';
     }
 
     let widthClasses = '';
@@ -48,25 +48,24 @@ const Content = ({ align = 'right', width = '48', containerClasses, contentClass
     }
 
     return (
-        <>
-            <Transition
-                as={Fragment}
-                show={open}
-                enter="transition ease-out duration-200"
-                enterFrom="transform opacity-0 scale-95"
-                enterTo="transform opacity-100 scale-100"
-                leave="transition ease-in duration-75"
-                leaveFrom="transform opacity-100 scale-100"
-                leaveTo="transform opacity-0 scale-95"
+        <Transition
+            as={"div"}
+            className="relative z-50"
+            show={open}
+            enter="transition ease-out duration-200"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+        >
+            <div
+                className={`absolute z-50 mt-2 rounded-md shadow-lg ${alignmentClasses} ${widthClasses} ${containerClasses}`}
+                onClick={() => setOpen(false)}
             >
-                <div
-                    className={`absolute z-50 mt-2 rounded-md shadow-lg ${alignmentClasses} ${widthClasses} ${containerClasses}`}
-                    onClick={() => setOpen(false)}
-                >
-                    <div className={`rounded-md ring-1 ring-black ring-opacity-5 ` + contentClasses}>{children}</div>
-                </div>
-            </Transition>
-        </>
+                <div className={`rounded-md ring-1 ring-black ring-opacity-5 ` + contentClasses}>{children}</div>
+            </div>
+        </Transition>
     );
 };
 
