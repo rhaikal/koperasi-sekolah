3c12b84840ddee3dec4fafcc7f7d1b5d974f3c04
rhaikal <rhaikal101@gmail.com>
2023-02-09 13:59:05 +0700

    menghapus coupun (table yang tidak diperlukan)

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,3 @@ use App\Http\Controllers\Dashboard\ProductController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::apiResource('products', ProductController::class);
-Route::apiResource('coupons', CouponController::class);
