5bc61968d76e8d2fd52c87ada9a675deb6fc272b
rhaikal <rhaikal101@gmail.com>
2023-02-20 10:30:49 +0700

    memperbaiki bug product list

--- resources/js/Pages/Product/Shop/Partials/ProductList.jsx
+++ resources/js/Pages/Product/Shop/Partials/ProductList.jsx
@@ -19,7 +19,7 @@ export default function ProductList({products, links}) {
                                 alt={product.imageAlt}
                                 className="h-full w-full rounded-lg object-cover object-center group-hover:opacity-75"
                                 />
-                                <ul className="product-hover absolute top-6 right-[-200px] cursor-pointer transition-all duration-700">
+                                <ul className="product-hover invisible absolute top-6 right-[-200px] cursor-pointer transition-all duration-700">
                                     <li className={`rounded border ${product.stock > 0 ? 'bg-indigo-100 border-indigo-400' : 'bg-rose-100 border-rose-400' }`}>
                                         <Link href={route("shop.show", product)}>
                                             <BiSearch className={`m-1 w-7 h-7 lg:h-5 lg:w-5 ${product.stock > 0 ? 'text-indigo-400' : 'text-rose-400'}`} />
