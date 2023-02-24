8238a89b64c90ef0ed5e3d040da0d4b03e29686a
rhaikal <rhaikal101@gmail.com>
2023-02-24 18:47:28 +0700

    mengubah delete dropdown link menjadi bahasa indonesia

--- resources/js/Components/Dashboard/Form/DeleteDropdownLink.jsx
+++ resources/js/Components/Dashboard/Form/DeleteDropdownLink.jsx
@@ -9,13 +9,13 @@ export default function DeleteDrodownLink({href}) {
         e.preventDefault()
 
         Swal.fire({
-            title: 'Are you sure?',
-            text: "You won't be able to revert this!",
+            title: 'Apakah anda yakin?',
+            text: "Anda tidak akan dapat mengembalikan ini!",
             icon: 'warning',
             showCancelButton: true,
             confirmButtonColor: '#3085d6',
             cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
+            confirmButtonText: 'Ya, hapus!'
         }).then((result) => {
             if (result.isConfirmed) {
               Inertia.delete(href);
